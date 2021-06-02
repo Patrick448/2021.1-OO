ca52c58332dad590a02d12673756317fef2c9281
Patrick <patrickcarvalho448@gmail.com>
2021-06-02 15:58:21 -0300

    Exercicio

--- 31-05-2021/src/main/java/Funcionario.java
+++ 31-05-2021/src/main/java/Funcionario.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Funcionario {
+import java.util.List;
+import java.util.Map;
+
+public class Funcionario {
+    private String nome;
+    private float salarioBruto;
+    private  float totalAcrescimos;
+    private float totalDescontos;
+    private List<Map<String, Float>> acrescimos;
+
+    public float calcularSalarioLiquido(){
+        return salarioBruto + totalAcrescimos - totalDescontos;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalarioBruto() {
+        return this.salarioBruto;
+    }
+
+    public void setSalarioBruto(float salarioBruto) {
+        this.salarioBruto = salarioBruto;
+    }
+
+    public float getTotalAcrescimos() {
+        return this.totalAcrescimos;
+    }
+
+    public void setTotalAcrescimos(float totalAcrescimos) {
+        this.totalAcrescimos = totalAcrescimos;
+    }
+
+    public float getTotalDescontos() {
+        return this.totalDescontos;
+    }
+
+    public void setTotalDescontos(float totalDescontos) {
+        this.totalDescontos = totalDescontos;
+    }
 }
--- 31-05-2021/src/main/java/Main.java
+++ 31-05-2021/src/main/java/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Ademilson Soares");
+        funcionario.setSalarioBruto(1500f);
+        funcionario.setTotalAcrescimos(150f);
+        funcionario.setTotalDescontos(60f);
+
+        Funcionario funcionario2 = new Funcionario();
+        funcionario2.setNome("Ana Lúcia Morais");
+        funcionario2.setSalarioBruto(1900f);
+        funcionario2.setTotalAcrescimos(210f);
+        funcionario2.setTotalDescontos(100f);
+
+        imprimirContracheque(funcionario);
+        imprimirContracheque(funcionario2);
+    }
+    public static void imprimirContracheque(Funcionario funcionario){
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("Salário bruto: " + funcionario.getSalarioBruto());
+        System.out.println("Acréscimos: " + funcionario.getTotalAcrescimos());
+        System.out.println("Descontos: " + funcionario.getTotalDescontos());
+        System.out.println("Salário líquido: " + funcionario.calcularSalarioLiquido());
+        System.out.println();
+    }
 }
